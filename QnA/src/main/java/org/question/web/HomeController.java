c17ac86318975a168482f89c15070ee94fb99ab9
LEE GyeongMin <min20@nhnnext.org>
2014-07-21 15:17:16 +0900

    04 Add HomeController

---
+++ QnA/src/main/java/org/question/web/HomeController.java
@@ -0,0 +1,15 @@
+package org.question.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/")
+	public String home() {
+	//	/home.jsp
+		return "home";
+	}
+	
+}
