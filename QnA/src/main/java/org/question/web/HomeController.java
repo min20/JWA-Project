d00187db09d9aad6c15d100602686b3b4abc045d
LEE GyeongMin <min20@nhnnext.org>
2014-07-25 00:01:54 +0900

    06 remove test. fin

--- QnA/src/main/java/org/question/web/HomeController.java
+++ QnA/src/main/java/org/question/web/HomeController.java
@@ -1,18 +1,13 @@
 package org.question.web;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	@RequestMapping("/")
 	public String home() {
-		logger.debug("logback setting success!");
-		
 	//	(webapp)/home.jsp과 연결된다.
 		return "home";
 	}
