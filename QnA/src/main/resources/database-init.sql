926f814b06c666702992be68b6991ef8bca86163
LEE GyeongMin <min20@nhnnext.org>
2014-07-28 15:24:25 +0900

    13 change database for taking longger messages

--- QnA/src/main/resources/database-init.sql
+++ QnA/src/main/resources/database-init.sql
@@ -1,10 +1,10 @@
 DROP TABLE IF EXISTS USERS;
 
 CREATE TABLE USERS (
-	userId		varchar(12)		NOT NULL,
-	password	varchar(12)		NOT NULL,
-	name		varchar(20)		NOT NULL,
-	email		varchar(50),
+	userId		varchar(20)		NOT NULL,
+	password	varchar(20)		NOT NULL,
+	name		varchar(50)		NOT NULL,
+	email		varchar(100),
 	PRIMARY KEY (userId) );
 	
 INSERT INTO USERS VALUES('min20', '1234', '이경민', 'min20@nhnnext.org');
