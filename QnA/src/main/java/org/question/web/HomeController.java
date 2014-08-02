3c695c98662e6b9fa377c9f10b5987d0de0153eb
LEE GyeongMin <min20@nhnnext.org>
2014-08-02 16:26:25 +0900

    16 Create TeseCase for HomeController. fin

--- QnA/src/main/java/org/question/web/HomeController.java
+++ QnA/src/main/java/org/question/web/HomeController.java
@@ -9,6 +9,8 @@ public class HomeController {
 	@RequestMapping("/")
 	public String home() {
 	//	(webapp)/home.jsp과 연결된다.
+	//  Return된 String 값을 이용해서 어느 페이지로 갈 지 결정이 된다.
+	//  그냥 "경로" 만 적으면 forward로, "redirect:경로" 를 적으면 redirect로 동작한다.
 		return "home";
 	}
 	
